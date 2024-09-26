ca20efab3cb305f6dd5debe63eb3944875a4182a
Shep <ads0041@mix.wvu.edu>
2024-09-26 15:58:45 -0400

    more, again

--- lab1_ws/lab1_pkg/include/lab1_pkg/cpp_header.hpp
+++ lab1_ws/lab1_pkg/include/lab1_pkg/cpp_header.hpp
@@ -1,1 +1,11 @@
 #include "lab1_pkg/cpp_header.hpp"
+
+class MyCustomNode : public rclcpp::Node
+{
+public:
+    MyCustomNode() : Node("my_node") {
+        RCLCPP_INFO(this->get_logger(), "TEST Cpp node");
+    }
+
+private:
+};
